8d71f070f88c06cdf6a61c28e5bb216301277296
kenlau5678 <kennew2013@gmail.com>
2024-02-01 12:26:23 +0800

    some optimization

---
+++ Assets/Script/CameraFollow.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform target;
+    public float smoothing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GameManager.camShake = GameObject.FindGameObjectWithTag("CameraShake").GetComponent<CameraShake>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        if (target != null)
+        {
+            if(transform.position != target.position)
+            {
+                Vector3 targetPos = target.position;
+                transform.position = Vector3.Lerp(transform.position, targetPos, smoothing);    
+            }
+        }
+    }
+}
