b1789227faed7724c68f6fc3077be2b7efd9c4f8
John2024Mo <lolrng666@gmail.com>
2024-03-06 18:32:35 +0800

    Create Boss script

--- Assets/Boss.cs
+++ Assets/Boss.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Boss : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform player;
+	public float health = 100f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public bool isFlipped = false;
+
+
+
+	public void LookAtPlayer()
+	{
+		
+		Vector3 flipped = transform.localScale;
+		flipped.z *= -1f;
+
+		if (transform.position.x > player.position.x && isFlipped)
+		{
+			transform.localScale = flipped;
+			transform.Rotate(0f, 180f, 0f);
+			isFlipped = false;
+		}
+		else if (transform.position.x < player.position.x && !isFlipped)
+		{
+			transform.localScale = flipped;
+			transform.Rotate(0f, 180f, 0f);
+			isFlipped = true;
+		}
+	}
+
+	public void TakeDamage(int damage)
+	{
+		health -= damage;
+		if(health <= 0)
+		{
+			Die();
+		}
+	}
+	void Die()
+	{
+		//Instantiate(deathEffect, transform.position, Quaternion.identity); //deathAnimator
+		
+
+		Destroy(gameObject);
+	}
 }
--- Assets/BossHandWalk.cs
+++ Assets/BossHandWalk.cs
@@ -8,17 +8,26 @@ public class BossHandWalk : StateMachineBehaviour
     public float attackTimer;
     public float minTime;
     public float maxTime;
+
+    public float distanceToPlayer;
+    public float speed;
+    Boss boss;
+    Transform player;
+	Rigidbody2D rb;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
        rand = Random.Range(0,2);//switch 2 different attack modes
        attackTimer = Random.Range(minTime,maxTime);//random attack time
+       player = GameObject.FindGameObjectWithTag("Player").transform;
+       rb = animator.GetComponent<Rigidbody2D>();
+       boss = animator.GetComponent<Boss>();
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-         if(attackTimer <= 0)
+        /* if(attackTimer <= 0)
         {
              if(rand == 0)
             {
@@ -32,8 +41,12 @@ public class BossHandWalk : StateMachineBehaviour
         else{
             attackTimer -= Time.deltaTime;
         }
-           
-       
+           */
+        distanceToPlayer = Vector2.Distance(animator.transform.position, player.position);
+        boss.LookAtPlayer();
+        Vector2 target = new Vector2(player.position.x, rb.position.y);
+		Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
+	    rb.MovePosition(newPos);
         
     }
 
