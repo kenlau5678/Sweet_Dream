253cd931d5853b94ea5df48048f68471d8e60f12
John2024Mo <lolrng666@gmail.com>
2024-04-04 18:49:30 +0800

    Boss2 Create
    创建了Boss2 的大概位置
    增加了Boss2射击镭射的位置
    初步写了镭射的攻击方式

---
+++ Assets/Boss2Shooting.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss2Shooting : MonoBehaviour
+{
+   public Transform leftShootPoint; // 左射击点
+    public Transform rightShootPoint; // 右射击点
+    public GameObject laserPrefab; // 镭射预制体
+
+    private GameObject currentLaser; // 当前的镭射对象
+
+    void Start()
+    {
+        ShootLaser();
+    }
+
+    void ShootLaser()
+    {
+        // 随机选择左右射击点
+        Transform shootPoint = Random.Range(0, 2) == 0 ? leftShootPoint : rightShootPoint;
+
+        // 生成镭射
+        currentLaser = Instantiate(laserPrefab, shootPoint.position, Quaternion.identity);
+
+        // 计算镭射的旋转角度
+        Vector3 directionToPlayer = (shootPoint.position - transform.position).normalized;
+        float angle = Mathf.Atan2(directionToPlayer.y, directionToPlayer.x) * Mathf.Rad2Deg - 90f;
+        currentLaser.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+
+        // 启动镭射旋转
+        RotateLaser();
+    }
+
+    void RotateLaser()
+    {
+        if (currentLaser != null)
+        {
+            // 旋转镭射
+            currentLaser.transform.Rotate(Vector3.forward * Time.deltaTime * 50f);
+
+            // 继续旋转
+            Invoke("RotateLaser", 0f);
+        }
+    }
+}
+    
+
+
+ 
