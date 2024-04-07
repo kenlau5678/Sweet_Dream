371ed9bffe5bb00f78b354c364fc7585cc9ab4f5
John2024Mo <lolrng666@gmail.com>
2024-04-07 23:40:32 +0800

    Create FireBall system
    增加了火球下落的机制，火球碰到玩家会销毁，

---
+++ Assets/FireBallManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBallManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] FireBallPrefabs;
+   public void SpawnFireBall()//生成
+    {
+        int temp = Random.Range(0, 10), r=0;
+        if(temp>=7)
+        {
+            r = 1;//BlueFireball
+        }
+        else
+        { r = 0;}//NormalBall
+        GameObject fireBall = Instantiate(FireBallPrefabs[r],transform);//直接生成在manager的子物件
+        fireBall.transform.position = new Vector3(Random.Range(-10f,13f), Random.Range(6f,10f), 0f);
+    }
+}
